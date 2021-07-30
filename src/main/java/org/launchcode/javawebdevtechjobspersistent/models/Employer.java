49bffee5028299b7bcfee8099f35fd15e4394543
KenzieQuery <william_query@comcast.com>
2021-07-30 08:57:53 -0600

    added jobs field to Employer and updates Job Model

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,14 +1,24 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 //needs @Entity annotation because it is referencing the AbstractEntity
 @Entity
 public class Employer extends AbstractEntity {
+
+    //Start Part 3: Within Employer, add a private property jobs of type List<Job> and initialize it to an empty ArrayList.
+    // After we set up the Job class to work with Employer objects, this list will represent the list of all
+    // items in a given job. We’ll do this in a bit.
+    //Use the @OneToMany and @JoinColumn annotations on the jobs list in Employer to declare the relationship
+    // between data tables.
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
+
     @NotBlank
     //add validation
     @Size(message = "Location must be between 2 and 250 characters.", min = 2, max = 250)
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
@@ -3,23 +3,30 @@ package org.launchcode.javawebdevtechjobspersistent.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+//Update Job Model Step 1: Update the class definition of Job to extend AbstractEntity.
+// Remove the redundant fields from Job.
 @Entity
 public class Job extends AbstractEntity{
 
     //Not needed because moved to AbstractEntity
-    @Id
-    @GeneratedValue
-    @Size(min=1, max=250, message = "ID must be between 1 and 250 characters")
-    private int id;
+//    @Id
+//    @GeneratedValue
+//    @Size(min=1, max=250, message = "ID must be between 1 and 250 characters")
+//    private int id;
+//
+//    @Size(min=2, max=250, message = "Name must be between 2 and 250 characters")
+//    private String name;
 
-    @Size(min=2, max=250, message = "Name must be between 2 and 250 characters")
-    private String name;
-
+    //Update Job Model Step 3: Add the @ManyToOne annotation on the field employer
+    @ManyToOne
+    //Update Job Model Step 2: Replace the type of the field employer to be of type Employer. You will also need to
+    // refactor the affected constructor and getter and setter that use this field.
     @Size(min=2, max=250, message = "Employer must be between 2 and 250 characters")
-    private String employer;
+    private Employer employer;
 
     @Size(min=2, max=250, message = "Description must be between 2 and 250 characters")
     private String skills;
